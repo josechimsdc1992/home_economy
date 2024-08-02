924a7b858cbe618db3900d4f5148bcc041c081c1
Jose Chim <52678389+josechim@users.noreply.github.com>
2024-08-01 19:59:52 -0600

    Test Static

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 import nextIntl from "next-intl/plugin";
 const nextConfig = {
-    
+    output: 'export',
 };
 
 const withNextIntl = nextIntl("./src/app/i18n.ts");
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,5 @@ import {redirect} from 'next/navigation';
 
 // This page only renders when the app is built statically (output: 'export')
 export default function RootPage() {
-  return(
-    <div>
-      Init Page
-    </div>
-  )
+  redirect('/en');
 }
